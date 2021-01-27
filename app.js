0ebdbbb0d0fa6c719b60d8ee59f2a907bc2cde39
halaabuqamar <halaabuqamar.1994@hotmail.com>
2021-01-27 16:31:28 +0200

    resturant

---
+++ app.js
@@ -0,0 +1,27 @@
+var nomeals=prompt("How Many Meals Did you buy?")
+
+var OrderDetails;
+
+if (nomeals >= 3 && nomeals <= 10) {
+    OrderDetails "Congrats you won a free meal"
+} 
+else if (nomeals >= 11 && nomeals <= 20) {
+    OrderDetails "Congrats you won TWO free meals"
+}
+
+else{
+    OrderDetails="Good Luck Next Time"
+}
+
+document.write(OrderDetails)
+document.getElementById("order").innerText=nomeals;
+
+var branch=prompt("Write the Branch Name:")
+var branchdetails; 
+if (branch == "abdoun" || branch == "7th circle" || branch == "swefieh" || branch == "abdallah ghosheh") {
+    
+} else {
+    branchdetails "you are not being a loyal customer :p"
+}
+
+alert(Are You LOvin It )
