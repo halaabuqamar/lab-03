c027839cb1b8176ac07de5fef8f96232a9e6d027
halaabuqamar <halaabuqamar.1994@hotmail.com>
2021-01-27 16:50:00 +0200

    done

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-var nomeals=prompt("How Many Meals Did you buy?")
+var nomeals = prompt("How Many Meals Did you buy?");
 
 var OrderDetails;
 
 if (nomeals >= 3 && nomeals <= 10) {
-    OrderDetails "Congrats you won a free meal"
-} 
+    OrderDetails = "Congrats you won a free meal";
+}
 else if (nomeals >= 11 && nomeals <= 20) {
-    OrderDetails "Congrats you won TWO free meals"
+    OrderDetails = "Congrats you won TWO free meals";
+}
+else {
+    OrderDetails = "Good Luck Next Time";
 }
 
-else{
-    OrderDetails="Good Luck Next Time"
-}
+document.write(OrderDetails);
+// document.getElementById("order").innerText = nomeals;
 
-document.write(OrderDetails)
-document.getElementById("order").innerText=nomeals;
-
-var branch=prompt("Write the Branch Name:")
-var branchdetails; 
+var branch = prompt("Write the Branch Name:")
+var branchdetails;
 if (branch == "abdoun" || branch == "7th circle" || branch == "swefieh" || branch == "abdallah ghosheh") {
-    
+
 } else {
-    branchdetails "you are not being a loyal customer :p"
+    branchdetails = "you are not being a loyal customer :p";
 }
 
-alert(Are You LOvin It )
+alert('Are You LOvin It');
